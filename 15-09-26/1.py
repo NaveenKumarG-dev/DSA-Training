617d1677eafa7cd993929a40fb4b9cacf53a2985
NaveenKumarG-dev <naveen13524g@gmail.com>
2026-09-15 11:07:27 +0530

    Add function to count occurrences of a target in an array

---
+++ 15-09-26/1.py
@@ -0,0 +1,15 @@
+
+#given an array and target x count the occuourance
+try:
+    arr = list(map(int,input().split()))
+    target = int(input())
+    count = 0
+
+    for i in arr:
+        if i == target:
+            count+=1
+
+    print(count)
+
+except Exception as e:
+    print(e)
